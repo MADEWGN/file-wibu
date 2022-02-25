9ed4c58bb37d3be81b3a9a767da54387ab31e1d3
madewgn <mad.ewgn2@gmail.com>
2022-02-25 18:51:00 +0800

    p

---
+++ tools.js
@@ -0,0 +1,69 @@
+const PDFDocument = require('pdfkit');
+const getStream = require('get-stream');
+const cheerio = require('cheerio');
+const axios = require('axios');
+const got = require('got');
+
+module.exports = {
+    get: async (url, option = {}) => {
+        try {
+            const response = await axios.get(url, option);
+            return response;
+        } catch (err) {
+            //get code status
+            const code = err.response.status;
+            if (code !== 200) {
+                const res = await axios.get(`https://bypass.kato-rest.us/url/${btoa(url)}`);
+
+                return res;
+            }
+        }
+    },
+
+    generatePDF: async (images) => {
+        try {
+            const doc = new PDFDocument({ autoFirstPage: false });
+
+            for (const image of images) {
+                if (image.endsWith(".gif")) continue;
+                const base64 = btoa(image);
+                const buffer = await require('got')(`https://bypass.kato-rest.us/url/${base64}`).buffer();
+                const img = doc.openImage(buffer);
+                doc.addPage({ size: [img.width, img.height] });
+                doc.image(img, 0, 0);
+            };
+
+            doc.end();
+
+            const pdfStream = await getStream.buffer(doc);
+            return pdfStream;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    },
+    getVideoSrc: async (url) => {
+        try {
+            const response = await got(url);
+            const $ = cheerio.load(response.body);
+            let source1 = $.html().search('"file":');
+            let source2 = $.html().search("'file':");
+            let source3 = $('source').attr('src');
+            let source4 = $('iframe').attr('src');
+
+            if (source1 !== -1) {
+                const end = $.html().indexOf('","');
+                return $.html().substring(source1 + 8, end);
+            } else if (source2 !== -1) {
+                const end = $.html().indexOf("','");
+                return $.html().substring(source2 + 8, end);
+            } else if (source3) {
+                return source3;
+            }
+            return "-";
+        } catch (error) {
+            return "-";
+        }
+    }
+
+};
